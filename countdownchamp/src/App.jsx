4f9edcac2ae1e56216ed6f6ef16fb25e5aa68a0b
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2017-11-12 19:21:18 -0500

    basic styling done

---
+++ countdownchamp/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div className="App-tittle">CountDown to December 25th, 2017</div>
+        <div>
+          <div className="Clock-days">14 Days</div>
+          <div className="Clock-hours">30 Hours</div>
+          <div className="Clock-minutes">15 Minutes</div>
+          <div className="Clock-seconds">20 Seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date'/>
+          <button>Submit</button>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default App;
