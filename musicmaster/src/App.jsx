74ec870b6536dc12df3901fa138806b2ce24ee53
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2017-11-16 22:57:55 -0500

    already playing tracks:)

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Profile from './Profile'
+import Gallery from './Gallery'
 import './App.css'
 import {Form, FormControl, FormGroup, Button, InputGroup, Glyphicon} from 'react-bootstrap'
 
@@ -9,7 +10,8 @@ class App extends Component{
     super(props);
     this.state = {
       query: '',
-      artist: null
+      artist: null,
+      tracks: []
     }
   }
 
@@ -17,8 +19,9 @@ class App extends Component{
     const BASE_URL= 'https://api.spotify.com/v1/search?'
     //const FETCH_URL: BASE_URL +'q='+ this.state.query
       //                + '&type=artist&limit=1'
-   const FETCH_URL= `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    var accessToken = 'BQCJikD_G7SXGfW2k0bmhj5fg43A2pDgIgBFP2FOgb__7Xs6T7hExxQddRBOFhgIeIfkIWTEsOmnxs4N0oLL_55K6dDsq9b9qaxL4FSJS6pXgmNJQ4QoZqz7ETlvQ-CyNff1GRkKGG2lMPWGd3V_slBlS2jAjQRRkvrLQVNbo1am00E'
+   let FETCH_URL= `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+   const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+    var accessToken = 'BQCqZjC-WnUFIa9vXnABYd-HjGHfF4mJjBeIl116aFAchiOs0D6vKumUH5Yr5AV2mXjfqFVJp8BgNxwsGAUSWlkDk3EcmVjD8lpsaILXo5XgDv9v24ubihAgi8xNt9yaP6fyNzif2AOT2mDfCFC60L0lcsiZptscOrV3LUpVukyG2uk'
     var myHeaders = new Headers();
 
     var myOptions = {
@@ -35,7 +38,15 @@ class App extends Component{
       .then(json => {
         const artist= json.artists.items[0];
         this.setState({artist});
-      })
+
+        FETCH_URL= `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, myOptions )
+        .then(response => response.json())
+        .then(json => {
+          const tracks = json.tracks;
+          this.setState({tracks});
+        })
+      });
   }
 
   render(){
@@ -60,12 +71,20 @@ class App extends Component{
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <Profile
-          artist = {this.state.artist}
-        />
-        <div className="Gallery">
-          Gallery
-        </div>
+        {
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile
+                artist = {this.state.artist}
+              />
+              <Gallery
+                tracks = {this.state.tracks}
+              />
+            </div>
+          : <div></div>
+        }
+
       </div>
 
     )
---
+++ musicmaster/src/Gallery.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class Gallery extends Component{
+  playAudio(previewUrl){
+    let audio = new Audio(previewUrl);
+    audio.play()
+  }
+  
+  render(){
+    const {tracks} = this.props;
+    return(
+      <div>
+        {tracks.map((track,k)=>{
+          const trackImg= track.album.images[0].url;
+          return(
+            <div
+              key={k}
+              className="track"
+              onClick={()=> this.playAudio(track.preview_url)}
+              >
+                <img
+                  src={trackImg}
+                  className="track-img"
+                  alt="tracks"
+                />
+                <p className = 'track-text'>
+                  {track.name}
+                </p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Gallery;
